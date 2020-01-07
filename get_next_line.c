4d9e16dcf8c1af6fa162b25ad74b458cec4c7607
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-07 10:31:34 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -2,15 +2,18 @@
 
 int		get_next_line(int fd, char **line)
 {
+	size_t size;
 	int fd;
 	char *buf;
+	int boolean;
+	int reading;
 
+	size = 0;
+	boolean = 0;
 	if (fd < 0)
 		return (0);
-	while (read(fd, buf, BUFFER_SIZE))
+	reading = read(fd, buf, BUFFER_SIZE);
+	while (reading)
 	{
-		line =
-		if (read(fd, buf, BUFFER_SIZE) == 0)
-			break;
-	}
+
 }
