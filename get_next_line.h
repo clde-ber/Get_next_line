74cb6125068194e1766822d9be206e2b1444aaff
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-07 10:12:10 +0100

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,10 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c)
+
+#endif
