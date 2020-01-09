74e900275bf2a81e0563485cc60ce3eb6485f36c
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-09 08:16:23 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -2,8 +2,9 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
+# include <stdlib.h>
 
 int		get_next_line(int fd, char **line);
-char	*ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c);
 
 #endif
