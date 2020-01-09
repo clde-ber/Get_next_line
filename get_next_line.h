6de69c4309b4f95b863b94a86402b4fe07fa12f2
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-09 15:31:46 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -5,6 +5,9 @@
 # include <stdlib.h>
 
 int		get_next_line(int fd, char **line);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_set_size(char c);
 
 #endif
