a2130b264ce93e69b3f40006fa7635dfe3c792ee
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-09 15:31:25 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,34 +1,73 @@
 #include "get_next_line.h"
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 3000
 
-static int	fd;
-static char	*buf;
+static char buf[BUFFER_SIZE];
 
 int		get_next_line(int fd, char **line)
 {
 	size_t tab_size;
-	size_t len_line;
-	int boolean;
+	size_t i;
 	int res;
 
 	tab_size = 0;
-	len_line = 0;
-	boolean = 0;
+	i = 0;
 	res = 0;
+//	while (i < BUFFER_SIZE - 1)
+//			buf[i++] = '\0';
+//	printf("buf au depart = %s\n", buf);
 	if (fd < 0)
 		return (-1);
-	while (read(fd, line[tab_size], BUFFER_SIZE))
+	i = 0;
+	while (read(fd, buf, ft_set_size(*buf)))
 	{
-		if (boolean == 0)
-			line[tab_size] = malloc(sizeof(char) * 1024);
-		else
-			line[tab_size] = malloc(sizeof(char) * len_line);
-		len_line = read(fd, line[tab_size], BUFFER_SIZE);
-		if (boolean)
-			line[tab_size] = ft_strrchr(line[tab_size], '\n');
-		boolean = 1;
+		printf("buffer %s\n", buf);
+		printf("i %zu\n", i);
+		if (!(line[tab_size] = malloc(sizeof(char) * i)))
+			return (0);
+		line[tab_size] = buf;
 		res = 1;
+		line[tab_size][++i] = '\0';
+		printf("%s\n", line[tab_size]);
 		tab_size++;
+		i = 0;
 	}
+	line[tab_size] = 0;
+	res = (res = 1) ? 0 : 1;
+	return (res);
+}#include "get_next_line.h"
+#define BUFFER_SIZE 3000
+
+static char buf[BUFFER_SIZE];
+
+int		get_next_line(int fd, char **line)
+{
+	size_t tab_size;
+	size_t i;
+	int res;
+
+	tab_size = 0;
+	i = 0;
+	res = 0;
+//	while (i < BUFFER_SIZE - 1)
+//			buf[i++] = '\0';
+//	printf("buf au depart = %s\n", buf);
+	if (fd < 0)
+		return (-1);
+	i = 0;
+	while (read(fd, buf, ft_set_size(*buf)))
+	{
+		printf("buffer %s\n", buf);
+		printf("i %zu\n", i);
+		if (!(line[tab_size] = malloc(sizeof(char) * i)))
+			return (0);
+		line[tab_size] = buf;
+		res = 1;
+		line[tab_size][++i] = '\0';
+		printf("%s\n", line[tab_size]);
+		tab_size++;
+		i = 0;
+	}
+	line[tab_size] = 0;
+	res = (res = 1) ? 0 : 1;
 	return (res);
 }
