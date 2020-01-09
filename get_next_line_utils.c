0e2ad75a44d5b8b8c02cb8a90a0354b19e698a09
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-09 15:32:08 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,5 +1,15 @@
 #include "get_next_line.h"
 
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+
+	str = (char *)s;
+	while (*str && *str != (char)c)
+		str++;
+	return (*str == c ? str : NULL);
+}
+
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
@@ -14,4 +24,41 @@ char	*ft_strrchr(const char *s, int c)
 		i--;
 	}
 	return (0);
+}
+
+/*char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (ft_strdup(""));
+	if (!(ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (0);
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		ptr[i + j] = s2[j];
+		j++;
+	}
+	ptr[i + j] = '\0';
+	return (ptr);
+}*/
+
+size_t	ft_set_size(char c)
+{
+	if (c == '\n')
+	{
+		printf("%zu\n", 1);
+		return (0);
+	}
+	printf("%zu\n", 0);
+	return (1);
 }
