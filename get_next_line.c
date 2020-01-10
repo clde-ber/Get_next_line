7896102609c4a162da3d2e50b778e6e36a170822
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-10 10:08:30 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,73 +1,35 @@
 #include "get_next_line.h"
 #define BUFFER_SIZE 3000
 
-static char buf[BUFFER_SIZE];
+static char 	buf[BUFFER_SIZE];
+static size_t	tab_size;
 
 int		get_next_line(int fd, char **line)
 {
-	size_t tab_size;
 	size_t i;
+	size_t j;
 	int res;
+	char *buf;
 
 	tab_size = 0;
 	i = 0;
+	j = 0;
 	res = 0;
-//	while (i < BUFFER_SIZE - 1)
-//			buf[i++] = '\0';
-//	printf("buf au depart = %s\n", buf);
-	if (fd < 0)
-		return (-1);
-	i = 0;
-	while (read(fd, buf, ft_set_size(*buf)))
+	buf = 0;
+	while (read(fd, buf, BUFFER_SIZE))
 	{
-		printf("buffer %s\n", buf);
-		printf("i %zu\n", i);
-		if (!(line[tab_size] = malloc(sizeof(char) * i)))
+		while (buf[i] != '\n' || buf[i] != '\0')
+			i++;
+		while (i < BUFFER_SIZE)
+			ft_get_line(i, buf, BUFFER_SIZE);
+		if (!(line[tab_size++] = malloc(sizeof(char) * (i + 1))))
 			return (0);
-		line[tab_size] = buf;
-		res = 1;
-		line[tab_size][++i] = '\0';
-		printf("%s\n", line[tab_size]);
-		tab_size++;
+		i = 0;
+		while (buf[i++] != '\n')
+			line[tab_size][j++] = buf[i];
+		line[tab_size][j + 1] = '\0';
+		return = (buf[i] = '\0') ? 0 : 1;
 		i = 0;
 	}
-	line[tab_size] = 0;
-	res = (res = 1) ? 0 : 1;
-	return (res);
-}#include "get_next_line.h"
-#define BUFFER_SIZE 3000
-
-static char buf[BUFFER_SIZE];
-
-int		get_next_line(int fd, char **line)
-{
-	size_t tab_size;
-	size_t i;
-	int res;
-
-	tab_size = 0;
-	i = 0;
-	res = 0;
-//	while (i < BUFFER_SIZE - 1)
-//			buf[i++] = '\0';
-//	printf("buf au depart = %s\n", buf);
-	if (fd < 0)
-		return (-1);
-	i = 0;
-	while (read(fd, buf, ft_set_size(*buf)))
-	{
-		printf("buffer %s\n", buf);
-		printf("i %zu\n", i);
-		if (!(line[tab_size] = malloc(sizeof(char) * i)))
-			return (0);
-		line[tab_size] = buf;
-		res = 1;
-		line[tab_size][++i] = '\0';
-		printf("%s\n", line[tab_size]);
-		tab_size++;
-		i = 0;
-	}
-	line[tab_size] = 0;
-	res = (res = 1) ? 0 : 1;
-	return (res);
+	return (-1);
 }
