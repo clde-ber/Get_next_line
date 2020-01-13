9a4b420acea601994e76fd34ffccc47f79b52a33
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-13 16:24:55 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,35 +1,36 @@
 #include "get_next_line.h"
-#define BUFFER_SIZE 3000
-
-static char 	buf[BUFFER_SIZE];
-static size_t	tab_size;
+#define BUFFER_SIZE 100
 
 int		get_next_line(int fd, char **line)
 {
 	size_t i;
 	size_t j;
-	int res;
-	char *buf;
+	size_t k;
+	static char buf[BUFFER_SIZE];
 
-	tab_size = 0;
 	i = 0;
 	j = 0;
-	res = 0;
-	buf = 0;
-	while (read(fd, buf, BUFFER_SIZE))
+	k = 0;
+	while (BUFFER_SIZE > 0 && read(fd, buf, BUFFER_SIZE) >= 0)
 	{
-		while (buf[i] != '\n' || buf[i] != '\0')
-			i++;
-		while (i < BUFFER_SIZE)
-			ft_get_line(i, buf, BUFFER_SIZE);
-		if (!(line[tab_size++] = malloc(sizeof(char) * (i + 1))))
-			return (0);
-		i = 0;
-		while (buf[i++] != '\n')
-			line[tab_size][j++] = buf[i];
-		line[tab_size][j + 1] = '\0';
-		return = (buf[i] = '\0') ? 0 : 1;
-		i = 0;
+		if (i < BUFFER_SIZE)
+		{
+			if (buf[i + 1] != '\n')
+			line[0] = ft_strdup(&buf[i]);i
+			i = ft_get_index(i, buf, BUFFER_SIZE);
+			if (!(*line = malloc(sizeof(char) * (i + 1))))
+				return (0);
+			printf("%zu\n", i);
+			while (j <= i)
+			{
+				line[0][j] = ft_get_letter(k, line, buf);
+				j++;
+				k++;
+			}
+			printf("%s\n", *line);
+			return (buf[j] == '\n') ? 1 : 0;
+		}
+		j = 0;
 	}
 	return (-1);
 }
