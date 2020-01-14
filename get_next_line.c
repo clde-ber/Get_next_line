b09028f090be2ca09b24b8cae1e112e977f376ea
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-14 20:15:13 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -16,13 +16,10 @@ int		get_next_line(int fd, char **line)
 		while (i < BUFFER_SIZE)
 		{
 			if (!line[0])
-				res = read(fd, buf, BUFFER_SIZE);
-			else
 			{
-				if (ft_read(line, fd, BUFFER_SIZE))
-					return(1);
-			}
-			i = ft_get_index(i, buf, res);
+				printf("premiereboucle");
+				res = read(fd, buf, BUFFER_SIZE);
+				i = ft_get_index(i, buf, res);
 				printf("longueur prise :%zu\n", BUFFER_SIZE - ft_strlen(&buf[i]));
 			if (!(*line = malloc(sizeof(char) * (i + BUFFER_SIZE - ft_strlen(&buf[i])))))
 				return (0);
@@ -35,11 +32,21 @@ int		get_next_line(int fd, char **line)
 				k++;
 			}
 			line[0][j] = '\0';
-		//	while (buf[k] != '\n')
-		//			line[0][j++] = buf[k++];
-		//	line[0][j] = '\0';
-				printf("%s\n", *line);
-				return (1);
+			printf("%s\n", *line);
+			return (1);
+			}
+			else
+			{
+				printf("a");
+				if (read(fd, buf, BUFFER_SIZE));
+				{
+					i = ft_strlen(buf) - ft_strlen(line);
+					line[0] = ft_strjoin(line[0], &buf[i]);
+					printf("%s\n", line[0]);
+					return(1);
+				}
+				return (0);
+			}
 		}
 	return (0);
 }
