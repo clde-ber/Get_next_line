4520cfcb03a2ef415e860d58331a108b8711a12b
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-14 17:27:12 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -3,6 +3,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+#include <stdio.h>
 
 int		get_next_line(int fd, char **line);
 char	*ft_strchr(const char *s, int c);
@@ -10,5 +11,7 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_get_index(size_t i, char *buf, size_t BUFFER_SIZE);
 char	ft_get_letter(size_t k, char **line,  char *buf);
+size_t	ft_strlen(char *str);
+size_t	ft_read(char **line, int fd, size_t BUFFER_SIZE);
 
 #endif
