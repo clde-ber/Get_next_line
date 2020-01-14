df4a118671061195449946a1a87db9b78f3980b2
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-14 20:16:30 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -8,7 +8,7 @@
 int		get_next_line(int fd, char **line);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_get_index(size_t i, char *buf, size_t BUFFER_SIZE);
 char	ft_get_letter(size_t k, char **line,  char *buf);
 size_t	ft_strlen(char *str);
