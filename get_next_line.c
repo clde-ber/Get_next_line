d9cfdec5108ee66135016a0021cb6b1a101bcb5c
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-14 17:26:44 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,28 @@ int		get_next_line(int fd, char **line)
 	size_t i;
 	size_t j;
 	size_t k;
+	size_t res;
 	static char buf[BUFFER_SIZE];
 
 	i = 0;
 	j = 0;
 	k = 0;
-	while (BUFFER_SIZE > 0 && read(fd, buf, BUFFER_SIZE) > 0)
-	{
+	res = 0;
 		while (i < BUFFER_SIZE)
 		{
-			i = ft_get_index(i, buf, BUFFER_SIZE);
-			if (!(*line = malloc(sizeof(char) * (i + 1))))
+			if (!line[0])
+				res = read(fd, buf, BUFFER_SIZE);
+			else
+			{
+				if (ft_read(line, fd, BUFFER_SIZE))
+					return(1);
+			}
+			i = ft_get_index(i, buf, res);
+				printf("longueur prise :%zu\n", BUFFER_SIZE - ft_strlen(&buf[i]));
+			if (!(*line = malloc(sizeof(char) * (i + BUFFER_SIZE - ft_strlen(&buf[i])))))
 				return (0);
-				printf("%zu\n", i);
+			printf("taille restante : %zu\n",  ft_strlen(&buf[i]));
+			printf("i : %zu\n", i);
 			while (j < i)
 			{
 				line[0][j] = ft_get_letter(k, line, buf);
@@ -26,11 +35,11 @@ int		get_next_line(int fd, char **line)
 				k++;
 			}
 			line[0][j] = '\0';
-			printf("%s\n", *line);
-			return (buf[k] == '\n') ? 1 : -1;
+		//	while (buf[k] != '\n')
+		//			line[0][j++] = buf[k++];
+		//	line[0][j] = '\0';
+				printf("%s\n", *line);
+				return (1);
 		}
-		j = 0;
-	}
 	return (0);
 }
-
