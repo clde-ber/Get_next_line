3e752312c89395c60c0047f33234105501e658ed
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-15 09:54:50 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,9 @@ int		get_next_line(int fd, char **line)
 	size_t i;
 	size_t j;
 	size_t k;
-	size_t res;
-	static char buf[BUFFER_SIZE];
+	int		res;
+	char buf[BUFFER_SIZE];
+	static char *stock;
 
 	i = 0;
 	j = 0;
@@ -15,16 +16,19 @@ int		get_next_line(int fd, char **line)
 	res = 0;
 		while (i < BUFFER_SIZE)
 		{
-			if (!line[0])
+			if (!stock)
 			{
 				printf("premiereboucle");
 				res = read(fd, buf, BUFFER_SIZE);
+				printf("%d\n", res);
 				i = ft_get_index(i, buf, res);
-				printf("longueur prise :%zu\n", BUFFER_SIZE - ft_strlen(&buf[i]));
-			if (!(*line = malloc(sizeof(char) * (i + BUFFER_SIZE - ft_strlen(&buf[i])))))
+				printf("%zu\n", i);
+			if (!(line[0] = malloc(sizeof(char) * (i + 1))))
 				return (0);
-			printf("taille restante : %zu\n",  ft_strlen(&buf[i]));
 			printf("i : %zu\n", i);
+			stock = (i < BUFFER_SIZE) ? ft_fill_stock(i, BUFFER_SIZE, &buf[i], BUFFER_SIZE - i) :
+				ft_strdup("");
+			printf("stock :%s\n", stock);
 			while (j < i)
 			{
 				line[0][j] = ft_get_letter(k, line, buf);
@@ -32,21 +36,28 @@ int		get_next_line(int fd, char **line)
 				k++;
 			}
 			line[0][j] = '\0';
-			printf("%s\n", *line);
-			return (1);
+			printf("*line %s\n", *line);
 			}
-			else
+			if (stock[0] == '\0' || stock[0])
 			{
 				printf("a");
-				if (read(fd, buf, BUFFER_SIZE));
+				if (stock[0] == '\0')
 				{
-					i = ft_strlen(buf) - ft_strlen(line);
-					line[0] = ft_strjoin(line[0], &buf[i]);
-					printf("%s\n", line[0]);
-					return(1);
+					if (!(stock = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+						return (0);
+					printf("read 2 %zd\n",read(fd, stock, BUFFER_SIZE));
+					stock[BUFFER_SIZE] = '\0';
+					printf("stock :%s\n", stock);
 				}
+	//			if (!(line[0] = malloc(sizeof(char) * (ft_strlen(stock) + 1))))
+	//				return (0);
+				printf("ft-strlenstock + 1 %zu\n", ft_strlen(stock) + 1);
+					line[0] = ft_strjoin(line[0], stock);
+					printf("line[0] %s\n", line[0]);
+					ft_del_stock(stock);
+					return(1);
+			}
 				return (0);
 			}
-		}
-	return (0);
+	return (-1);
 }
