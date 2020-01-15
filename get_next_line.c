d2a8421eea8804a0f4f0e429f6e1335600abb8e2
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-15 10:54:04 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -14,21 +14,21 @@ int		get_next_line(int fd, char **line)
 	j = 0;
 	k = 0;
 	res = 0;
-		while (i < BUFFER_SIZE)
+		if (BUFFER_SIZE > 0)
 		{
 			if (!stock)
 			{
-				printf("premiereboucle");
+	//			printf("premiereboucle");
 				res = read(fd, buf, BUFFER_SIZE);
-				printf("%d\n", res);
+	//			printf("%d\n", res);
 				i = ft_get_index(i, buf, res);
-				printf("%zu\n", i);
+	//			printf("%zu\n", i);
 			if (!(line[0] = malloc(sizeof(char) * (i + 1))))
 				return (0);
-			printf("i : %zu\n", i);
+	//		printf("i : %zu\n", i);
 			stock = (i < BUFFER_SIZE) ? ft_fill_stock(i, BUFFER_SIZE, &buf[i], BUFFER_SIZE - i) :
 				ft_strdup("");
-			printf("stock :%s\n", stock);
+	//		printf("stock :%s\n", stock);
 			while (j < i)
 			{
 				line[0][j] = ft_get_letter(k, line, buf);
@@ -36,28 +36,26 @@ int		get_next_line(int fd, char **line)
 				k++;
 			}
 			line[0][j] = '\0';
-			printf("*line %s\n", *line);
 			}
 			if (stock[0] == '\0' || stock[0])
 			{
-				printf("a");
+	//			printf("a");
 				if (stock[0] == '\0')
 				{
 					if (!(stock = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 						return (0);
-					printf("read 2 %zd\n",read(fd, stock, BUFFER_SIZE));
+					read(fd, stock, BUFFER_SIZE);
 					stock[BUFFER_SIZE] = '\0';
-					printf("stock :%s\n", stock);
+	//				printf("stock :%s\n", stock);
 				}
 	//			if (!(line[0] = malloc(sizeof(char) * (ft_strlen(stock) + 1))))
 	//				return (0);
-				printf("ft-strlenstock + 1 %zu\n", ft_strlen(stock) + 1);
+	//			printf("ft-strlenstock + 1 %zu\n", ft_strlen(stock) + 1);
 					line[0] = ft_strjoin(line[0], stock);
-					printf("line[0] %s\n", line[0]);
+					printf("%s\n", line[0]);
 					ft_del_stock(stock);
 					return(1);
 			}
-				return (0);
-			}
+		}
 	return (-1);
 }
