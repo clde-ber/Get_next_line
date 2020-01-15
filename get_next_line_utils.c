96c964bebbb2394d5e7498e7bf999e1a157cb27e
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-15 09:55:40 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -32,9 +32,42 @@ size_t	ft_get_index(size_t i, char *buf, size_t BUFFER_SIZE)
 	size_t j;
 
 	j = 0;
-	while (buf[j] != '\n')
+	while (j < BUFFER_SIZE)
+	{
+		if (buf[j] == '\n')
+			break;
 		j++;
+	}
 	return (j);
+}
+
+char	*ft_fill_stock(size_t i, size_t BUFFER_SIZE, char *buf, size_t len)
+{
+	size_t j;
+	char *stock;
+
+	j = 0;
+	if (!(stock = malloc(sizeof(char) * (BUFFER_SIZE - i + 1))))
+		return (0);
+	while (j < len)
+	{
+		stock[j] = buf[j];
+		j++;
+	}
+	stock[j] = '\0';
+	return (stock);
+}
+
+void	ft_del_stock(char *stock)
+{
+	size_t i;
+
+	i = 0;
+	while (stock[i])
+	{
+		stock[i] = '\0';
+		i++;
+	}
 }
 
 char	ft_get_letter(size_t k, char **line,  char *buf)
@@ -79,8 +112,12 @@ char	*ft_strjoin(char *s1, char *s2)
 	char	*ptr;
 	i = 0;
 	j = 0;
-	if (!s1 || !s2)
-		return (ft_strdup(""));
+	if (!s1 && !s2)
+		return (0);
+	if (!s1 || s1[0] == '\0')
+		return (ft_strdup(s2));
+	if (!s2 || s2[0] == '\0')
+		return (ft_strdup(s1));
 	if (!(ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (0);
 	while (s1[i])
