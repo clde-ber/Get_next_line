4dd0e07ca21911bcdb558f1f1e917ebbe77a53c4
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-18 16:32:44 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 
-int	ft_find_n(char *buf, size_t size)
+int		find_n(char *buf, size_t size)
 {
 	size_t i;
 
@@ -14,25 +14,38 @@ int	ft_find_n(char *buf, size_t size)
 	return (-1);
 }
 
-void	ft_clear_left(char *left, size_t size)
+char	*ft_create_a_clear_left(char *left, size_t size)
 {
 	int k;
 
-	k = ft_find_n(left, size);
-	if (k != -1)
+	if (left)
 	{
-		ft_memmove(left, left + k + 1, ft_strlen(left));
-		left[ft_strlen(left)] = '\0';
+		k = find_n(left, size);
+		if (k != -1)
+		{
+			ft_memmove(left, left + k + 1, ft_strlen(left));
+			left[ft_strlen(left)] = '\0';
+		}
+		else
+			left[0] = '\0';
+		return (left);
 	}
 	else
+	{
+		if (!(left = malloc(sizeof(char) * (size + 1))))
+			return (0);
 		left[0] = '\0';
+		return (left);
+	}
 }
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*tmp_src;
 	unsigned char	*tmp_dst;
+	size_t			i;
 
+	i = 0;
 	if (!dst && !src)
 		return (NULL);
 	if (src <= dst)
@@ -43,40 +56,14 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 			tmp_dst[len] = tmp_src[len];
 	}
 	else
-		ft_memcpy(dst, src, len);
-	return (dst);
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t i;
-
-	if (!dst && !src)
-		return (NULL);
-	i = 0;
-	while (i < n)
 	{
-		((char *)dst)[i] = ((char *)src)[i];
-		i++;
+		while (i < len)
+		{
+			((char *)dst)[i] = ((char *)src)[i];
+			i++;
+		}
 	}
 	return (dst);
-}
-
-char	*ft_strdup(char *s1)
-{
-	size_t	i;
-	char	*ptr;
-
-	i = 0;
-	if (!(ptr = malloc(sizeof(char) * (ft_strlen(s1) + 1))))
-		return (0);
-	while (s1[i])
-	{
-		ptr[i] = s1[i];
-		i++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
 }
 
 size_t	ft_strlen(char *str)
@@ -93,28 +80,24 @@ char	*ft_strjoin(char *s1, char *s2)
 {
 	size_t	i;
 	size_t	j;
+	size_t	dont_free_left;
 	char	*ptr;
-	i = 0;
-	j = 0;
+
+	i = -1;
+	j = -1;
+	dont_free_left = 0;
+	if (s2[0] == '\0')
+		dont_free_left = 1;
 	if (!s1 && !s2)
 		return (0);
-	if (!s1 || s1[0] == '\0')
-		return (ft_strdup(s2));
-	if (!s2 || s2[0] == '\0')
-		return (ft_strdup(s1));
 	if (!(ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (0);
-	while (s1[i])
-	{
+	while (s1[++i])
 		ptr[i] = s1[i];
-		i++;
-	}
-	while (s2[j])
-	{
+	while (s2[++j])
 		ptr[i + j] = s2[j];
-		j++;
-	}
 	ptr[i + j] = '\0';
-	free(s1);
+	if (dont_free_left == 0)
+		free(s1);
 	return (ptr);
 }
