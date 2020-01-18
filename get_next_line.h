83c4e115adfce145df875e9f9fae074dca64a18f
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-18 13:42:49 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,10 @@
 #include <stdio.h>
 
 int		get_next_line(int fd, char **line);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_get_index(size_t i, char *buf, size_t BUFFER_SIZE);
-char	ft_get_letter(size_t k, char **line,  char *buf);
 size_t	ft_strlen(char *str);
-size_t	ft_read(char **line, int fd, size_t BUFFER_SIZE);
-char 	*ft_fill_stock(size_t i, size_t BUFFER_SIZE, char *buf, size_t len);
-void	ft_del_stock(char *stock);
 char	*ft_strdup(char *s1);
-int	ft_find_n(char *buf, size_t size);
+int		ft_find_n(char *buf, size_t size);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	ft_clear_left(char *left, size_t size);
