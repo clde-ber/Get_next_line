db56d2ae09651b862448539387dbd503a014274f
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-18 14:34:02 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,23 +1,15 @@
 #include "get_next_line.h"
-#define BUFFER_SIZE 1000000
+#define BUFFER_SIZE 0
 
 int		get_next_line(int fd, char **line)
 {
 	size_t i;
-	size_t j;
 	char buf[BUFFER_SIZE + 1];
 	static char *left;
 
 	i = 0;
 	buf[0] = '\0';
-	if (fd < 0 || read(fd, buf, 0))
-		return (-1);
-	if (!left)
-	{
-		if (!(left = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
-			return (-1);
-		left[0] = '\0';
-	}
+	left = (!left) ? ft_create_left(left, BUFFER_SIZE) : left;
 	line[0] = ft_strdup(left);
 	ft_clear_left(left, BUFFER_SIZE);
 	if (ft_find_n(line[0], ft_strlen(line[0])) != -1)
@@ -26,15 +18,9 @@ int		get_next_line(int fd, char **line)
 		printf("%s\n", line[0]);
 		return (1);
 	}
-	while (ft_find_n(buf, BUFFER_SIZE) == -1)
-	{
-		if (!(i = read(fd, buf, BUFFER_SIZE)))
-			break ;
-		buf[i] = '\0';
+	while (ft_find_n(buf, BUFFER_SIZE) == -1 && (i = read(fd, buf, BUFFER_SIZE)))
 		line[0] = ft_strjoin(line[0], buf);
-	}
-	if (i == 0)
-		line[0] = ft_strjoin(line[0], buf);
+	line[0] = (i == 0) ? ft_strjoin(line[0], buf) : line[0];
 	if (ft_find_n(line[0], ft_strlen(line[0])) != -1)
 	{
 		line[0][ft_find_n(line[0], ft_strlen(line[0]))] = '\0';
@@ -42,6 +28,5 @@ int		get_next_line(int fd, char **line)
 		printf("%s\n", line[0]);
 		return (1);
 	}
-	else
-		return (0);
+	return (fd < 0 || read(fd, buf, 0) || left == NULL || BUFFER_SIZE == 0) ? -1 : 0;
 }
